f757e6dae0df6a084408d6967ab4a89a202864ff
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2020-08-13 14:29:06 +0800

    Updata
    1.目錄功能
    2.頁面樣式修改

--- js/main.js
+++ js/main.js
@@ -7,10 +7,8 @@
     ga(function() {
         let trackers = ga.getAll();
         $('#flow').append("<p>在線人數："+trackers.length+"</p>");
-        $('#flow').append("<p>本日人氣："+trackers.length+"</p>");
-        $('#flow').append("<p>累積人氣："+trackers.length+"</p>");
-        console.table(trackers);
-      });
+    });
+
 $(document).ready(function () {
     $( window ).scroll(function() {
         if($(window).width()>764)
@@ -37,37 +35,47 @@ $(document).ready(function () {
             }
         }
     });
-    //頁面切換動作
-    const page_max=2;
+    //顯示幾個頁面
+    const page_max=1;
     let page=1;
-    $("#"+page).css("background","red");
+    for(let i=1;i<=page_max;i++)
+    {
+        $('#page').append('<span id="'+i+'">'+i+'</span>');
+    }
+    //頁面切換動作
+    $("#"+page).css("background","#90b76c6b");
     $(".left,.right").click(function (){
         if($(this).attr('class')=="left"){
             if(page>1){
-                $("#"+page).css("background","burlywood");
+                $("#"+page).css("background","#deb8878a");
                 page--;
                 $("iframe").attr("src","article/page_"+page+".html");
                 $('html, body').scrollTop($(".header").height()*0.75);
-                $("#"+page).css("background","red");
+                $("#"+page).css("background","#90b76c6b");
             }
         }else{
             if(page<page_max){
-                $("#"+page).css("background","burlywood");
+                $("#"+page).css("background","#deb8878a");
                 page++;
                 $("iframe").attr("src","article/page_"+page+".html");
                 $('html, body').scrollTop($(".header").height()*0.75);
-                $("#"+page).css("background","red");
+                $("#"+page).css("background","#90b76c6b");
             }
         }
     });
     $(".page > span").click(function () {
-        $("#"+page).css("background","burlywood");
+        $("#"+page).css("background","#deb8878a");
         page=$(this).attr("id");
         $("iframe").attr("src","article/page_"+page+".html");
         $('html, body').scrollTop($(".header").height()*0.75);
-        $("#"+page).css("background","red");
+        $("#"+page).css("background","#90b76c6b");
     });
+    //回到最上面
     $("nav >img").click(function () { 
         $('html, body').scrollTop($(".header").height()*0.75);    
+    });
+    //分類,排序
+    $("#daily,#technology,#2020year").click(function () {
+        $(this).nextAll("ul").children().toggle( "slow" );
     });
 });
