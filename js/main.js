cd6e35e9472f1de290a80f78dceb6d9726648289
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2020-08-13 10:25:00 +0800

    Test GA

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+/*Google Analytics*/
+( function ( i , s , o , g , r , a , m ){ i [ 'GoogleAnalyticsObject' ]= r ; i [ r ]= i [ r ]|| function (){ ( i [ r ]. q = i [ r ]. q ||[]). push ( arguments )}, i [
+    r ]. l = 1 * new Date (); a = s . createElement ( o ), m = s . getElementsByTagName ( o )[ 0 ]; a . async = 1 ; a . src = g ; m . parentNode . insertBefore ( a , m ) })( window , document , 'script' 
+    , 'https://www.google-analytics.com/analytics.js' , 'ga' ); 
+    ga ( 'create' , 'UA-174663250-1' , 'auto' );
+    ga ( 'send' , ' pageview' );
+    ga(function() {
+        let trackers = ga.getAll();
+        $('#flow').append("<p>在線人數："+trackers.length+"</p>");
+        $('#flow').append("<p>本日人氣："+trackers.length+"</p>");
+        $('#flow').append("<p>累積人氣："+trackers.length+"</p>");
+        console.table(trackers);
+      });
 $(document).ready(function () {
     $( window ).scroll(function() {
         if($(window).width()>764)
