d1a3302784eb382459e2e0e58ddb108531c9bfa3
JerryBULL <65128468+jerryBULL@users.noreply.github.com>
2020-08-14 17:31:48 +0800

    version1.1.0
    1.將iframe改成JQuery的load
    2.新增文章
    3.樣式微調
    4.目錄/文章區/頁底改成區塊方便管理

--- js/main.js
+++ js/main.js
@@ -8,8 +8,10 @@
         let trackers = ga.getAll();
         $('#flow').append("<p>在線人數："+trackers.length+"</p>");
     });
-
 $(document).ready(function () {
+    $("#content").load("../block/article.html");
+    $("#menu").load("../block/menu.html");
+    $("#footer").load("../block/footer.html");
     $( window ).scroll(function() {
         if($(window).width()>764)
         {
@@ -35,47 +37,8 @@ $(document).ready(function () {
             }
         }
     });
-    //顯示幾個頁面
-    const page_max=1;
-    let page=1;
-    for(let i=1;i<=page_max;i++)
-    {
-        $('#page').append('<span id="'+i+'">'+i+'</span>');
-    }
-    //頁面切換動作
-    $("#"+page).css("background","#90b76c6b");
-    $(".left,.right").click(function (){
-        if($(this).attr('class')=="left"){
-            if(page>1){
-                $("#"+page).css("background","#deb8878a");
-                page--;
-                $("iframe").attr("src","article/page_"+page+".html");
-                $('html, body').scrollTop($(".header").height()*0.75);
-                $("#"+page).css("background","#90b76c6b");
-            }
-        }else{
-            if(page<page_max){
-                $("#"+page).css("background","#deb8878a");
-                page++;
-                $("iframe").attr("src","article/page_"+page+".html");
-                $('html, body').scrollTop($(".header").height()*0.75);
-                $("#"+page).css("background","#90b76c6b");
-            }
-        }
-    });
-    $(".page > span").click(function () {
-        $("#"+page).css("background","#deb8878a");
-        page=$(this).attr("id");
-        $("iframe").attr("src","article/page_"+page+".html");
-        $('html, body').scrollTop($(".header").height()*0.75);
-        $("#"+page).css("background","#90b76c6b");
-    });
     //回到最上面
     $("nav >img").click(function () { 
         $('html, body').scrollTop($(".header").height()*0.75);    
-    });
-    //分類,排序
-    $("#daily,#technology,#2020year").click(function () {
-        $(this).nextAll("ul").children().toggle( "slow" );
     });
 });
